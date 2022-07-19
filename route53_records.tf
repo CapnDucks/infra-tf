de0fd068dd284790be8373327feb3bd3c67a12be
Capn Duck <git@capnduck.com>
2022-07-19 12:40:41 -0700

    Add www

--- route53_records.tf
+++ route53_records.tf
@@ -54,6 +54,15 @@ module "capnduck_records" {
     },
     {
       allow_overwrite = true
+      name            = "www"
+      type            = "CNAME"
+      ttl             = "60"
+      records = [
+        "capnduck.ddns.net.",
+      ]
+    },
+    {
+      allow_overwrite = true
       name            = ""
       type            = "NS"
       ttl             = "172800"
