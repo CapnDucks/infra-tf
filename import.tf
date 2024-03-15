3883844c816b65e00eec8fa882d0a088e6aff8b7
J Elliott <75694124+jelliottio@users.noreply.github.com>
2024-03-15 15:05:36 -0700

    Remove imports. (#1)
    * Remove imports.
    
    * Update the initial commit.
    
    ---------
    
    Co-authored-by: Cap'n Duck <coder@capnduck.com>

--- import.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_cloudwatch_log_group.database-1
-  id = "/aws/rds/cluster/database-1"
-}
-
-import {
-  to = aws_cloudwatch_log_group.youmybitch
-  id = "/aws/logs/youmybitch"
-}
