de1d667d120354913e76407e84fcf0dbbc543bdd
jelliott.io <jelliott.io@capnduck.com>
2024-03-21 15:34:59 -0700

    Importing capnduck.com Route53 zone.

--- data.tf
+++ data.tf
@@ -43,6 +43,6 @@ data "aws_subnets" "public" {
 data "aws_route53_zone" "this" {
   count = local.use_existing_route53_zone ? 1 : 0
 
-  name         = local.domain_name
+  name         = local.setting["domain_name"]
   private_zone = false
 }
