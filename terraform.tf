0381fee29da5fb260087d9ffa2fdd042b39825a3
jelliott.io <jelliott.io@capnduck.com>
2024-05-23 09:07:07 -0700

    Updating terraform, providers and adding GITHUB_TOKEN for actions.

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "= 1.7.5"
+  required_version = "= 1.8.3"
   backend "s3" {
     bucket         = "capnduck-terraform-tfstate"
     key            = "terraform.tfstate"
@@ -11,27 +11,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 5.41.0"
+      version = "~> 5.0"
     }
     dns = {
       source  = "hashicorp/dns"
-      version = "= 3.4.0"
+      version = "~> 3.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "= 4.0.5"
+      version = "~> 4.0"
     }
     external = {
       source  = "hashicorp/external"
-      version = "= 2.3.3"
+      version = "~> 2.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "= 3.2.2"
+      version = "~> 3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "= 3.6.0"
+      version = "~> 3.0"
     }
   }
 }
