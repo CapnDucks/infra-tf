880a6ffa2edd74d9798e842c808e8013370ff241
J Elliott <75694124+jelliottio@users.noreply.github.com>
2024-11-07 09:41:07 -0800

    Update AWS provider. (#33)
    * Update AWS provider.
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "= 1.9.8"
+  required_version = "1.9.8"
   backend "s3" {
     bucket         = "capnduck-terraform-tfstate"
     key            = "terraform.tfstate"
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.72.1"
+      version = "5.74.0"
     }
     dns = {
       source  = "hashicorp/dns"
