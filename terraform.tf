15157bb4d3840226f9f5c184500c09dedd35490d
J Elliott <75694124+jelliottio@users.noreply.github.com>
2025-03-12 15:05:00 -0700

    Updating to TF 1.11.2. (#45)
    * Updating to TF 1.11.2.
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.11.0"
+  required_version = "1.11.2"
   backend "s3" {
     bucket         = "capnduck-terraform-tfstate"
     key            = "terraform.tfstate"
