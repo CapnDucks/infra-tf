c6cfcefc76caf3fcbdf8c7dd70a559ea2dc7ad0f
J Elliott <75694124+jelliottio@users.noreply.github.com>
2025-04-14 16:01:35 -0700

    Cleanup and add S3 state locking. (#49)
    * Cleanup and add S3 state locking.
    
    * Terraform fmt.

--- s3.tf
+++ s3.tf
@@ -1,12 +1,10 @@
 resource "aws_s3_bucket" "tfstate" {
   bucket = local.setting["s3_bucket"]
-
 }
 
 resource "aws_s3_bucket_acl" "tfstate" {
   bucket = aws_s3_bucket.tfstate.id
   acl    = local.setting["s3_bucket_acl"]
-
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
