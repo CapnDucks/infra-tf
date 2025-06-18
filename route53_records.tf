b58a1108d3f4aaac3b9fb12934104b6df6d9dac2
jelliott.io <jelliott.io@capnduck.com>
2025-06-18 16:02:54 -0700

    Add CNAME.

--- route53_records.tf
+++ route53_records.tf
@@ -108,6 +108,15 @@ module "capnduck_records" {
     },
     {
       allow_overwrite = true
+      name            = "apt"
+      type            = "CNAME"
+      ttl             = "60"
+      records = [
+        "capnduck.ddns.net.",
+      ]
+    },
+    {
+      allow_overwrite = true
       name            = ""
       type            = "NS"
       ttl             = "172800"
